df44123833d48688110c0ae51c1522098babdce3
Hendrik Scholtz <135076120+Hendrik2319@users.noreply.github.com>
2024-09-03 22:17:04 +0200

    refactor: improved code style

--- src/net/schwarzbaer/java/lib/gui/HSColorChooser.java
+++ src/net/schwarzbaer/java/lib/gui/HSColorChooser.java
@@ -597,11 +597,11 @@ public final class HSColorChooser {
 		private HashMap<ActionCommands, Vector<JComponent>> map;
 
 		public Disabler() {
-			map = new HashMap<ActionCommands,Vector<JComponent>>();
+			map = new HashMap<>();
 		}
 		
 		public void setCareFor(ActionCommands actionCommand) {
-			map.put(actionCommand,new Vector<JComponent>());
+			map.put(actionCommand,new Vector<>());
 		}
 
 		public void setCareFor(ActionCommands[] values) {
